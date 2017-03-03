53b1a5599647fce7bb9b70e437f8efc05f7c12a6
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-03 19:20:57 +0000

    ajax form working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,35 +8,5 @@ $(() => {
     }
   });
 
-  $('#save').on('click', function(event) {
-    event.preventDefault();
-    console.log('CLICKEDDDD');
-    alert('SDOFOSDJIOF');
 
-    const $form = $(this).closest('form');
-    console.log(this);
-    // const $title = $form.find('.locationTitle').val();
-    // const $desc = $form.find('.locationDlesc').val();
-    // const $image = $form.find('.locationDesc').val();
-
-    $.ajax({
-      console.log('test');
-      method: 'POST',
-      url: 'http://localhost:8080/maps/map_id/location',
-      data: $form.serialize()
-
-    }).then((data) => {
-
-      alert('TESTING');
-      console.log('DATAAAAAAA', data);
-      res.send("yay?");
-
-    }).fail(function(xhr, err) {
-      console.log(err);
-      alert('LAME');
-    });
-
-
-
-  });
 });
--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -2,8 +2,8 @@ let map;
 let marker;
 let infowindow;
 let messagewindow;
-let long = 0;
-let lats = 0;
+var long = 0;
+var lats = 0;
 
 function initMap() {
   let california = {
@@ -29,9 +29,6 @@ function initMap() {
       position: event.latLng,
       map: map
     });
-    $('#save').on('click', (event) => {
-      console.log('test');
-    });
 
 
     google.maps.event.addListener(marker, 'click', function() {
@@ -40,5 +37,38 @@ function initMap() {
 
     console.log(marker.getPosition().lat(), marker.getPosition().lng());
 
+
+    $('#infoForm').submit( function(event) {
+      event.preventDefault();
+      console.log(this);
+      alert('NEW?');
+
+      const $form = $(this).closest('form');
+      const $title = $form.find('.locationTitle').val();
+      const $desc = $form.find('.locationDesc').val();
+      const $image = $form.find('.locationImage').val();
+
+      let testing = $('.locationTitle, .locationDesc, .locationImage').serialize();
+      console.log(testing);
+
+      $.ajax({
+        method: 'POST',
+        url: 'http://localhost:8080/maps/map_id/location',
+        data: $form.serialize()
+
+      }).then((data) => {
+
+        alert('TESTING');
+        console.log('DATAAAAAAA', data);
+        res.send("yay?");
+
+      }).fail(function(xhr, err) {
+        console.log(err);
+        alert('LAME');
+      });
+
+    });
+
   });
+
 }
