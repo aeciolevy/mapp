53f4c90a948d8b25822c1e2833819d69f4c77df7
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-04 22:50:20 +0000

    save styling

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -5,12 +5,50 @@
   window.initMap = function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {});
-
     currentMap = map;
+
     let infowindow = new google.maps.InfoWindow({
-      content: document.getElementById('infoBox')
+      // content: document.getElementById('infoBox')
     });
 
+    let addMarkerCenterMap = function(data) {
+      var newBoundary = new google.maps.LatLngBounds();
+      for (let i = 0; i < data.length; i++) {
+        var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+        let marker = new google.maps.Marker({
+          position: latLng,
+          map: currentMap,
+          id: data[i].id,
+          title: data[i].title
+        });
+        marker.addListener('click', function() {
+          infowindow.setContent(
+            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+            '<div class="modal-header">' +
+            '<h3 class="modal-title">' + data[i].title + '</h3>' +
+            '</div>' +
+            '<div class="modal-body">' +
+            "<p>I mean, why would a poptart want to live inside a toaster, Rick? I mean, that would be like the scariest place for them to live. You know what I mean? S-S-Samantha. Holy crap, Morty, run! Run for your life, Morty, run! I-I've never seen that thing before in my life, Morty, I don't even know what the hell it is! We-we gotta get out of here, Morty, it's gonna kill us! We're gonna die, Morty! We're gonna die! Listen, Morty, I hate to break it to you but what people call love is just a chemical reaction that compels animals to breed.</p>" +
+            '<div class="coordinates">  ' + data[i].latitude +' , ' + data[i].longitude + '</div>' +
+            '</div>' +
+            '<div class="modal-footer">' +
+            '<button id="delete-btn" type="button" class="btn btn-danger btn-xs">Delete</button>' +
+            '<button id="save-btn" type="button" class="btn btn-primary btn-xs" data-dismiss="modal">Save</button>' +
+            '<button id="edit-btn" type="button" class="btn btn-primary btn-xs" data-dismiss="modal">Edit</button>' +
+            '</div>'
+          );
+          infowindow.open(map, marker);
+        });
+
+        newBoundary.extend(marker.position);
+
+      }
+      currentMap.fitBounds(newBoundary);
+    };
+    $.getJSON("/locations/").then(function(data) {
+      addMarkerCenterMap(data);
+      // getMarkerContent(data);
+    });
 
     google.maps.event.addListener(map, 'click', function(event) {
       let marker = new google.maps.Marker({
@@ -22,7 +60,6 @@
         infowindow.open(map, marker);
       });
     });
-    ////////////
 
     var card = document.getElementById('pac-card');
     var input = document.getElementById('pac-input');
@@ -33,6 +70,7 @@
 
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
+
 
 
   };
@@ -64,48 +102,32 @@
 
 
 
-    let addMarkerCenterMap = function(data) {
-      var newBoundary = new google.maps.LatLngBounds();
-      for (let i = 0; i < data.length; i++) {
-        var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: currentMap,
-          id: data[i].id,
-          title: data[i].title
-        });
-        newBoundary.extend(marker.position);
+    // let addMarkerCenterMap = function(data) {
+    //   var newBoundary = new google.maps.LatLngBounds();
+    //   for (let i = 0; i < data.length; i++) {
+    //     var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+    //     var marker = new google.maps.Marker({
+    //       position: latLng,
+    //       map: currentMap,
+    //       id: data[i].id,
+    //       title: data[i].title
+    //     });
+    //     marker.addListener('click', function() {
+    //       alert(marker.title);
+    //       infowindow.setContent(marker.title);
+    //       infowindow.open(currentMap);
+    //     });
+    //
+    //     newBoundary.extend(marker.position);
+    //
+    //   }
+    //   currentMap.fitBounds(newBoundary);
+    // };
 
-      }
-      currentMap.fitBounds(newBoundary);
-    };
-
-
-    let getMarkerContent = (data) => {
-      for (let i = 0; i < data.length; i++) {
-
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(data[i].latitude, data[i].longitude),
-          map: currentMap,
-          title: data[i].title
-        });
-
-        google.maps.addListener(marker, 'click', function(marker, i) {
-          // return function() {
-          console.log("TEST: ");
-          infowindow.setContent(data[i].description);
-          infowindow.open(currentMap, marker);
-          // }
-        });
-
-      }
-
-    };
-
-    $.getJSON("/locations/").then(function(data) {
-      addMarkerCenterMap(data);
-      // getMarkerContent(data);
-    });
+    // $.getJSON("/locations/").then(function(data) {
+    //   addMarkerCenterMap(data);
+    //   // getMarkerContent(data);
+    // });
 
 
 
