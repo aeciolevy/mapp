1bcc01e2fb5002386d025610847c8821341c8067
Aecio Levy <aeciolevy@gmail.com>
2017-03-04 21:51:38 +0000

    Rename the file and add script to render a modal

---
+++ public/scripts/scriptMaps.js
@@ -0,0 +1,97 @@
+(function() {
+  var currentMarker;
+  var currentMap;
+  var currentInfoWindow;
+  var allMarkers = [];
+  window.initMap = function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+    });
+    currentMap = map;
+    var infowindow = new google.maps.InfoWindow({
+      content: document.getElementById('infoBox')
+    });
+    currentInfoWindow = infowindow;
+
+    google.maps.event.addListener(map, 'click', function(event) {
+      let marker = new google.maps.Marker({
+        position: event.latLng,
+        map: map
+      });
+      allMarkers.push(marker);
+      google.maps.event.addListener(marker, 'click', function() {
+        currentMarker = marker;
+        let markerID = marker.get('id');
+        console.log('Marker ID:', markerID);
+        infowindow.open(map, marker);
+      });
+    });
+  };
+
+  $(function(){
+    $('#infoForm').submit(function(event) {
+      event.preventDefault();
+      let $map = $('#map');
+      let $data = $map.data();
+      const $form = $(this);
+      const $title = $('.locationTitle').val();
+      const $desc = $('.locationDesc').val();
+      const $image = $('.locationImage').val();
+      $.ajax({
+        method: 'POST',
+        url: `/locations/${$data.mapid}`,
+        data: {
+          title: $title,
+          description: $desc,
+          image: $image,
+          latitude: currentMarker.getPosition().lat(),
+          longitude: currentMarker.getPosition().lng()
+        }
+      }).then();
+      this.reset();
+      currentInfoWindow.close();
+    });
+
+    let getLocationData = function(data, marker){
+      let obj = data[0];
+      let infowindow = new google.maps.InfoWindow();
+      infowindow.setContent(
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+        `<h4 class="modal-title"> ${obj.title} </h4>` +
+        '<div class="modal-body">' +
+          `<p> ${obj.description}&hellip;</p>` +
+        '</div>' +
+        '<div class="modal-footer">' +
+          '<button type="button" class="btn btn-primary">Save changes</button>' +
+        '</div>'
+        );
+      infowindow.open(currentMap, marker);
+    };
+
+    let addMarkerCenterMap = function(data){
+      var newBoundary = new google.maps.LatLngBounds();
+
+      for (let i = 0; i < data.length; i++){
+        let latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+        let marker = new google.maps.Marker({
+          position: latLng,
+          map: currentMap,
+          id: data[i].id,
+          title: data.title
+        });
+        marker.addListener('click', function(){
+          $.getJSON(`/locations/${marker.id}`).then(data => {
+            getLocationData(data, marker);
+          });
+        });
+        allMarkers.push(marker);
+        newBoundary.extend(marker.position);
+      }
+      console.log(allMarkers);
+      currentMap.fitBounds(newBoundary);
+    };
+    let $map = $('#map');
+    let $data = $map.data();
+    $.getJSON(`/locations/?show=maps&mapId=${$data.mapid}`).then(addMarkerCenterMap);
+  });
+})();
+
